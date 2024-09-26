edf469339f55a68978364af4f42328c91e91501d
박민재 <96967422+Cfctor9@users.noreply.github.com>
2024-09-26 17:35:42 +0900

    [level 1] Title: 모의고사, Time: 8.40 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 75.5 MB, 시간: 7.63 ms
+메모리: 76.9 MB, 시간: 8.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 16:36:05
+2024년 09월 26일 17:35:39
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,30 +1,32 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] answers) {
         int[][] pattern = {
             {1,2,3,4,5},
             {2,1,2,3,2,4,2,5},
-            {3,3,1,1,2,2,4,4,5,5,}
-        };
+            {3,3,1,1,2,2,4,4,5,5,} 
+        }; //패턴 정리
         
-        int[] scores = new int[3];
+        int[] result = new int[3]; // 3명 동점일수 있으니까
         
-        for(int i = 0; i<answers.length; i++){
-            for(int j = 0; j<scores.length;j++){
+        for(int i=0;i< answers.length;i++){
+            for(int j=0;j<pattern.length ;j++){
                 if(answers[i] == pattern[j][i%pattern[j].length]){
-                    scores[j]++;
+                    result[j]++;
                 }
             }
         }
-        int maxscore = Arrays.stream(scores).max().getAsInt();
+        
+        int maxScore = Arrays.stream(result).max().getAsInt();
+        
         ArrayList<Integer> answer = new ArrayList<>();
-        for(int i = 0; i<scores.length;i++){
-            if(scores[i] == maxscore){
+        for(int i=0; i<result.length;i++){
+            if(result[i] == maxScore){
                 answer.add(i+1);
             }
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
+        
     }
 }
