b8d085c607a153060b63db3a2a6fec1977271299
박민재 <96967422+Cfctor9@users.noreply.github.com>
2025-01-13 13:18:30 +0900

    [Silver III] Title: 순열 사이클, Time: 420 ms, Memory: 32504 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,22 +1,20 @@
-def solution(v):
-    visited[v] = 1
-    next = arr[v]
-    if visited[next] == 0:
-        solution(next)
-    return
+import sys
+sys.setrecursionlimit(2000) #최대 재귀를 늘려줘야 런타임 에러를 피할 수 있다
 
+def dfs(x): #DFS 함수 정의
+    visited[x] = True #방문 체크
+    number = numbers[x] #다음 방문 장소
+    if not visited[number]: #방문하지 않았다면
+        dfs(number) #재귀
 
-t = int(input())
-arr = [0]
-for _ in range(t):
-    cnt = 0
-    n = int(input())
-    visited = [0] * (n + 1)
-    arr = list(map(int, input().split()))
-    # arr = [0] + list(map(int, input().split()))
-    arr.insert(0, 0)
-    for i in range(1, n + 1):
-        if visited[i] == 0:
-            solution(i)
-            cnt += 1
-    print(cnt)
+for _ in range(int(input())):
+    N = int(input())
+    numbers = [0] + list(map(int, input().split()))
+    visited = [True] + [False] * N #방문여부확인용
+    result = 0
+    
+    for i in range(1, N+1):
+        if not visited[i]: #방문하지 않았다면
+            dfs(i) #DFS실행
+            result += 1 #결과값 += 1
+    print(result)
