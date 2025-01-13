093d3095fb6034058e5ef72c69d4c99144ba066e
박민재 <96967422+Cfctor9@users.noreply.github.com>
2025-01-13 13:13:10 +0900

    [Silver III] Title: 순열 사이클, Time: 476 ms, Memory: 32536 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,30 +1,20 @@
-def make_graph(n,perm):
-    graph = [[] for _ in range(n+1)]
-    for i in range(1,n+1):
-        graph[i].append(perm[i-1])
-    return graph
+import sys
+sys.setrecursionlimit(10**6)
 
-def dfs(graph, start, visited):
-    stack = [start]
-    while stack:
-        current = stack.pop()
-        if not visited[current]:
-            visited[current] = True
-            stack.extend(i for i in graph[current] if not visited[i])
+def dfs(x, visited, perm):
+    visited[x] = True
+    next = perm[x-1]
+    if not visited[next]:
+        dfs(next, visited, perm)
 
-def make_result(n,perm):
-    graph = make_graph(n,perm)
-    visited = [False]*(n+1)
-    cnt = 0
-    for i in range(1,n+1):
-        if not visited[i]:
-            dfs(graph,i,visited)
-            cnt += 1
-    return cnt
-        
-t = int(input())
-for _ in range(t):
+for _ in range(int(input())):
     n = int(input())
-    perm = list(map(int,input().split()))
-    result = make_result(n,perm)
-    print(result)
+    perm = list(map(int, input().split()))
+    visited = [False] * (n+1)
+    cnt = 0
+    
+    for i in range(1, n+1):
+        if not visited[i]:
+            dfs(i, visited, perm)
+            cnt += 1
+    print(cnt)
