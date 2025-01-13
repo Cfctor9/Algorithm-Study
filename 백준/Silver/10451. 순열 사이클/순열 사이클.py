ab94f3e1b2a4554dadd8971fa415d7224f048b31
박민재 <96967422+Cfctor9@users.noreply.github.com>
2025-01-13 13:14:53 +0900

    [Silver III] Title: 순열 사이클, Time: 216 ms, Memory: 112304 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,20 +1,22 @@
-import sys
-sys.setrecursionlimit(10**6)
+def solution(v):
+    visited[v] = 1
+    next = arr[v]
+    if visited[next] == 0:
+        solution(next)
+    return
 
-def dfs(x, visited, perm):
-    visited[x] = True
-    next = perm[x-1]
-    if not visited[next]:
-        dfs(next, visited, perm)
 
-for _ in range(int(input())):
-    n = int(input())
-    perm = list(map(int, input().split()))
-    visited = [False] * (n+1)
+t = int(input())
+arr = [0]
+for _ in range(t):
     cnt = 0
-    
-    for i in range(1, n+1):
-        if not visited[i]:
-            dfs(i, visited, perm)
+    n = int(input())
+    visited = [0] * (n + 1)
+    arr = list(map(int, input().split()))
+    # arr = [0] + list(map(int, input().split()))
+    arr.insert(0, 0)
+    for i in range(1, n + 1):
+        if visited[i] == 0:
+            solution(i)
             cnt += 1
     print(cnt)
